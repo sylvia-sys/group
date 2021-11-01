ea4b5ce4b11f10119061918545824b55ad36dcb0
sylvia-sys <sylviakezia2000@gmail.com>
2021-11-01 08:14:11 +0300

    reinitialized files

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<title> my blog </title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+        <h1><a href="/post">my first post</a></h1>
+        <p>rojrjjgrog rkjefjeofjef  rejoj3oj j3ofj eojfier
+            jffjofjeofjejgjgiggrigrigrigrig
+            fuhfeoferfueroherfhrofjpofjelhkbhevhekvekvdkvhb
+            bjekjeorjlegjoeg ejgeojeofjeo keeojgoejgej kknkvn 
+            jekhwofjwofjegheig drgoegjeogjeoj kfhojpwgjwjgeoghief
+            ejkengkegnelgekbgjebgdrkr bekvnknegegnkrne kkrnkgnekek
+            jgengnbknbkbkfbn jbfknkbnkfnfkbnfnbf rkbfkbfkbneeje,nrkb
+</p>
+</article>
+<a href="/">go back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<title> my blog </title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+        <h1><a href="/post">my first post</a></h1>
+        <p>rojrjjgrog rkjefjeofjef  rejoj3oj j3ofj eojfier
+            jffjofjeofjejgjgiggrigrigrigrig
+            fuhfeoferfueroherfhrofjpofjelhkbhevhekvekvdkvhb
+            bjekjeorjlegjoeg ejgeojeofjeo keeojgoejgej kknkvn 
+            jekhwofjwofjegheig drgoegjeogjeoj kfhojpwgjwjgeoghief
+            ejkengkegnelgekbgjebgdrkr bekvnknegegnkrne kkrnkgnekek
+            jgengnbknbkbkfbn jbfknkbnkfnfkbnfnbf rkbfkbfkbneeje,nrkb
+</p>
+</article>
+<article>
+        <h1><a href="/post">my second post</a></h1>
+        <p>rojrjjgrog rkjefjeofjef  rejoj3oj j3ofj eojfier
+            jffjofjeofjejgjgiggrigrigrigrig
+            fuhfeoferfueroherfhrofjpofjelhkbhevhekvekvdkvhb
+            bjekjeorjlegjoeg ejgeojeofjeo keeojgoejgej kknkvn 
+            jekhwofjwofjegheig drgoegjeogjeoj kfhojpwgjwjgeoghief
+            ejkengkegnelgekbgjebgdrkr bekvnknegegnkrne kkrnkgnekek
+            jgengnbknbkbkfbn jbfknkbnkfnfkbnfnbf rkbfkbfkbneeje,nrkb
+</p>
+</article>
+<article>
+        <h1><a href="/post">my third post</a>s</h1>
+        <p>rojrjjgrog rkjefjeofjef  rejoj3oj j3ofj eojfier
+            jffjofjeofjejgjgiggrigrigrigrig
+            fuhfeoferfueroherfhrofjpofjelhkbhevhekvekvdkvhb
+            bjekjeorjlegjoeg ejgeojeofjeo keeojgoejgej kknkvn 
+            jekhwofjwofjegheig drgoegjeogjeoj kfhojpwgjwjgeoghief
+            ejkengkegnelgekbgjebgdrkr bekvnknegegnkrne kkrnkgnekek
+            jgengnbknbkbkfbn jbfknkbnkfnfkbnfnbf rkbfkbfkbneeje,nrkb
+</p>
+</article>
+</body>
